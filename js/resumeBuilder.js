226d52f55c23ba27e2ea15aa88ef91d54cd4db89
carolinass <carolzinhacabral@gmail.com>
2016-11-18 21:54:25 -0200

    Quick fixes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,6 +4,7 @@ This is empty on purpose! Your code to build the resume will go here.
 
 // skills, jobs, projects, project images, schools and onlineCourses arrays
 
+var data = "%data%";
 var bio = {};
 
 bio.name = "Carol Cabral";
@@ -20,21 +21,21 @@ bio.skills = ["Delivering things", "Seriously funny", "Hard working"];
 bio.biopic = "images/carol.jpg";
 bio.display = function() {
 
-    HTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
-    HTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
-    HTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-    HTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
-    HTMLgithub = HTMLgithub.replace("%data%", bio.contacts.github);
-    HTMLtwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-    HTMLlocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    HTMLbioPic = HTMLbioPic.replace("%data%", bio.biopic);
-    HTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    HTMLheaderName = HTMLheaderName.replace(data, bio.name);
+    HTMLheaderRole = HTMLheaderRole.replace(data, bio.role);
+    HTMLmobile = HTMLmobile.replace(data, bio.contacts.mobile);
+    HTMLemail = HTMLemail.replace(data, bio.contacts.email);
+    HTMLgithub = HTMLgithub.replace(data, bio.contacts.github);
+    HTMLtwitter = HTMLtwitter.replace(data, bio.contacts.twitter);
+    HTMLlocation = HTMLlocation.replace(data, bio.contacts.location);
+    HTMLbioPic = HTMLbioPic.replace(data, bio.biopic);
+    HTMLwelcomeMsg = HTMLwelcomeMsg.replace(data, bio.welcomeMessage);
 
-    $("#topContacts").append(HTMLmobile);
-    $("#topContacts").append(HTMLemail);
-    $("#topContacts").append(HTMLgithub);
-    $("#topContacts").append(HTMLtwitter);
-    $("#topContacts").append(HTMLlocation);
+    $("#topContacts, #footerContacts").append(HTMLmobile);
+    $("#topContacts, #footerContacts").append(HTMLemail);
+    $("#topContacts, #footerContacts").append(HTMLgithub);
+    $("#topContacts, #footerContacts").append(HTMLtwitter);
+    $("#topContacts, #footerContacts").append(HTMLlocation);
 
     $("#header").prepend(HTMLheaderRole);
     $("#header").prepend(HTMLheaderName);
@@ -43,7 +44,7 @@ bio.display = function() {
 
     $("#header").append(HTMLskillsStart);
     bio.skills.forEach(function(item) {
-        $("#header").append(HTMLskills.replace("%data%", item));
+        $("#header").append(HTMLskills.replace(data, item));
     });
 };
 
@@ -64,10 +65,10 @@ work = {
     display: function() {
         work.jobs.forEach(function(item) {
             $("#workExperience").append(HTMLworkStart);
-            $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", item.employer) + HTMLworkTitle.replace("%data%", item.title));
-            $(".work-entry:last").append(HTMLworkDates.replace("%data%", item.dates));
-            $(".work-entry:last").append(HTMLworkLocation.replace("%data%", item.location));
-            $(".work-entry:last").append(HTMLworkDescription.replace("%data%", item.description));
+            $(".work-entry:last").append(HTMLworkEmployer.replace(data, item.employer) + HTMLworkTitle.replace(data, item.title));
+            $(".work-entry:last").append(HTMLworkDates.replace(data, item.dates));
+            $(".work-entry:last").append(HTMLworkLocation.replace(data, item.location));
+            $(".work-entry:last").append(HTMLworkDescription.replace(data, item.description));
         });
     }
 };
@@ -87,70 +88,57 @@ projects = {
     display: function() {
         projects.projects.forEach(function(item) {
             $("#projects").append(HTMLprojectStart);
-            $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", item.title));
-            $(".project-entry:last").append(HTMLprojectDates.replace("%data%", item.dates));
-            $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", item.description));
+            $(".project-entry:last").append(HTMLprojectTitle.replace(data, item.title));
+            $(".project-entry:last").append(HTMLprojectDates.replace(data, item.dates));
+            $(".project-entry:last").append(HTMLprojectDescription.replace(data, item.description));
             item.images.forEach(function(image) {
-                $(".project-entry:last").append(HTMLprojectImage.replace("%data%", image));
+                $(".project-entry:last").append(HTMLprojectImage.replace(data, image));
             });
         });
     }
 };
 
 education = {
-    schools: [
-        {
-            name: "Universidade Federal de Campina Grande",
-            location: "Campina Grande, PB",
-            degree: "Bachelor of Science",
-            majors: ["Computer Science"],
-            dates: "2010 - 2015",
-            url: "http://ufcg.edu.br"
-        }
-    ],
-    onlineClasses: [
-        {
-            title: "Front-End Web Developer Nanodegree",
-            school: "Udacity",
-            dates: "2016 - 2017",
-            url: "http://udacity.com"
-        }
-    ],
+    schools: [{
+        name: "Universidade Federal de Campina Grande",
+        location: "Campina Grande, PB",
+        degree: "Bachelor of Science",
+        majors: ["Computer Science"],
+        dates: "2010 - 2015",
+        url: "http://ufcg.edu.br"
+    }],
+    onlineCourses: [{
+        title: "Front-End Web Developer Nanodegree",
+        school: "Udacity",
+        dates: "2016 - 2017",
+        url: "http://udacity.com"
+    }],
     display: function() {
         education.schools.forEach(function(item) {
             $("#education").append(HTMLschoolStart);
-            $(".education-entry:last").append((HTMLschoolName.replace("%data%", item.name) + HTMLschoolDegree.replace("%data%", item.degree)).replace("#", item.url));
-            $(".education-entry:last").append(HTMLschoolDates.replace("%data%", item.dates));
-            $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", item.location));
+            $(".education-entry:last").append((HTMLschoolName.replace(data, item.name) + HTMLschoolDegree.replace(data, item.degree)).replace("#", item.url));
+            $(".education-entry:last").append(HTMLschoolDates.replace(data, item.dates));
+            $(".education-entry:last").append(HTMLschoolLocation.replace(data, item.location));
             item.majors.forEach(function(major) {
-                $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", major));
+                $(".education-entry:last").append(HTMLschoolMajor.replace(data, major));
             });
         });
         $("#education").append(HTMLonlineClasses);
-        education.onlineClasses.forEach(function(item) {
+        education.onlineCourses.forEach(function(item) {
             $("#education").append(HTMLschoolStart);
-            $('.education-entry:last').append((HTMLonlineTitle.replace("%data%", item.title) + HTMLonlineSchool.replace("%data%", item.school)).replace("#", item.url));
-            $('.education-entry:last').append(HTMLonlineDates.replace("%data%", item.dates));
-            $('.education-entry:last').append(HTMLonlineURL.replace("%data%", item.url).replace("#", item.url));
+            $('.education-entry:last').append((HTMLonlineTitle.replace(data, item.title) + HTMLonlineSchool.replace(data, item.school)).replace("#", item.url));
+            $('.education-entry:last').append(HTMLonlineDates.replace(data, item.dates));
+            $('.education-entry:last').append(HTMLonlineURL.replace(data, item.url).replace("#", item.url));
         });
     }
 };
 
-var addMap = function() {
+var displayMap = function() {
     $("#mapDiv").append(googleMap);
-};
-
-var addFooter = function() {
-    $("#footerContacts").append(HTMLmobile);
-    $("#footerContacts").append(HTMLemail);
-    $("#footerContacts").append(HTMLgithub);
-    $("#footerContacts").append(HTMLtwitter);
-    $("#footerContacts").append(HTMLlocation);
 };
 
 bio.display();
 work.display();
 projects.display();
 education.display();
-addMap();
-addFooter();
+displayMap();
